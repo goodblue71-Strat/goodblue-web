4a0bc19b2a8290c850ffe5749ba923b267776a4a
goodblue71-Strat <goodblue71@gmail.com>
2026-01-22 21:27:58 -0800

    Refactor SWOTPage to redirect and remove Frameworks
    Removed Frameworks component and added redirection to /comp.

--- app/tryapp/page.tsx
+++ app/tryapp/page.tsx
@@ -1,24 +1,23 @@
 "use client";
+
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import Frameworks from "@/components/Frameworks";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function SWOTPage() {
   const pathname = usePathname();
+  const router = useRouter();
   const showCTA = pathname !== "/tryapp" && pathname !== "/app/tryapp";
+
+  useEffect(() => {
+    router.push("/comp");
+  }, [router]);
 
   return (
     <div className="min-h-screen flex flex-col bg-gray-50 text-gray-800">
-      {/* Navbar */}
       <Navbar showCTA={showCTA} />
-
-      {/* Main Section with Frameworks in the middle */}
-      <main className="flex-grow flex items-center justify-center px-4 py-8">
-        <Frameworks />
-      </main>
-
-      {/* Footer */}
+      {/* Nothing renders here because we navigate away */}
       <Footer />
     </div>
   );
